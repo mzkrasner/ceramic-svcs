cfe52c85e9beaf83f8f1f58cf972c8a00f4468f4
Mark Krasner <mzkrasner@gmail.com>
2024-04-04 09:41:01 -0400

    Adding authenticating with key did

--- packages/emit/main.ts
+++ packages/emit/main.ts
@@ -1,7 +1,10 @@
 import { CeramicClient } from "@ceramicnetwork/http-client";
 import { ComposeClient } from "@composedb/client";
 import { RuntimeCompositeDefinition } from "@composedb/types";
-import { definition } from "./generated/definition";
+import { definition } from "./generated/definition.js";
+import { Ed25519Provider } from "key-did-provider-ed25519";
+import { getResolver } from "key-did-resolver";
+import { DID } from "dids";
 import { EthereumWebAuth, getAccountId } from "@didtools/pkh-ethereum";
 import { Wallet } from "ethers";
 import { DIDSession } from "did-session";
@@ -13,25 +16,25 @@ async function main() {
     definition: definition as RuntimeCompositeDefinition,
   });
 
-  const wallet = Wallet.createRandom();
-  const accountId = await getAccountId(
-    wallet,
-    wallet.address
-  );
-  const authMethod = await EthereumWebAuth.getAuthMethod(
-    wallet,
-    accountId
-  );
-  const session = await DIDSession.get(accountId, authMethod, {
-    resources: composeClient.resources,
-  });
+  // Authenticate with Key DID
+  let seed_array: Uint8Array;
+  let seed = crypto.getRandomValues(new Uint8Array(32));
+  seed_array = seed;
 
-  //@ts-ignore
-  await ceramicClient.setDID(session.did);
-  //@ts-ignore
-  await composeClient.setDID(session.did);
+  const provider = new Ed25519Provider(seed_array);
+  const did = new DID({ provider, resolver: getResolver() });
+  await did.authenticate();
+  console.log("DID:", did.id);
+
+  ceramicClient.did = did;
+  composeClient.setDID(did);
+
+  // //@ts-ignore
+  // await ceramicClient.setDID(did);
+  // //@ts-ignore
+  // await composeClient.setDID(session.did);
 
   console.log("isAuth:", composeClient);
 }
 
-main().then(() => { })
+main().then(() => {});
